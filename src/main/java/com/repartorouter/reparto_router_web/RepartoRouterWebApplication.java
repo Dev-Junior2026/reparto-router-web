a58e69991e8479a554d02ccfc75fff3841caf0fd
dev luis pacheco <lapr271090@gmail.com>
2026-07-30 21:24:27 +0200

    feat: estructura inicial del proyecto Spring Boot

---
+++ src/main/java/com/repartorouter/reparto_router_web/RepartoRouterWebApplication.java
@@ -0,0 +1,13 @@
+package com.repartorouter.reparto_router_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepartoRouterWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepartoRouterWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/repartorouter/reparto_router_web/RepartoRouterWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repartorouter.reparto_router_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepartoRouterWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
